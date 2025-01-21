21125057f3f2853792c6055c6f2c3dd1fa0ee94a
mh026818 <129567630+mh026818@users.noreply.github.com>
2025-01-21 15:39:07 -0600

    Create capstone_gui.js

---
+++ capstone_gui.js
@@ -0,0 +1,202 @@
+const processes = [];
+process_to_run = 0;
+
+class process
+{
+    constructor(bucket1, bucket2, bucket3, bucket4, bucket5, bucket6, bucket7, bucket8)
+    {
+        this.bucket1 = bucket1;
+        this.bucket2 = bucket2;
+        this.bucket3 = bucket3;
+        this.bucket4 = bucket4;
+        this.bucket5 = bucket5;
+        this.bucket6 = bucket6;
+        this.bucket7 = bucket7;
+        this.bucket8 = bucket8;
+    }
+}
+
+function start_screen()
+{
+    let s = [];
+    //s.push(``);
+    s.push(`<center><canvas id="canvas" width=100% height=100% style="border:0px solid #FFFFFF;"></canvas></center>`);
+    //add button commands
+    s.push(`<div>
+            <button class="button_change" onclick="change_process()">Change Process</button>
+            <button class="button_create" onclick="create_new_process()">Create New Process</button>
+            <button class="button_run" onclick="run_process()">Run</button></div>`);
+
+    var content = document.getElementById("content");
+    if(content)
+    {
+        content.innerHTML = s.join('');
+    }
+    
+    document.body.style.backgroundColor = "lightgrey";
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    //grey gradient specifics 
+    const color_gradient = ctx.createRadialGradient(325, 400, 450, 325, 400, 15);
+    color_gradient.addColorStop(1, "lightgrey");
+    color_gradient.addColorStop(0, "grey");
+
+    ctx.fillStyle = color_gradient;
+    ctx.fillRect(0, 0, 650, 800);// 850 is half the screen for x
+    ctx.fillStyle = "black";
+    x = 40; // 1st row left shift
+    x2 = x + 190; // 2nd row left shift
+    x3 = x2 + 220; // 3rd row left shift
+    y = 40; // 1st column down shift
+    y2 = 70; // 2nd column down shift
+    y3 = 100; // 3rd column down shift
+    
+    //print the process names alter font size later as needed...
+    ctx.font = "40px Georgia";
+    ctx.fillText("Process 1:", x, 50);
+    ctx.fillText("Process 2:", x, 200);
+    ctx.fillText("Process 3:", x, 350);
+    ctx.fillText("Process 4:", x, 500);
+    ctx.fillText("Process 5:", x, 650);
+
+    //print the process' information
+    ctx.font = "20px Georgia";
+    //print process 1 information
+    ctx.fillText("Bucket 1: 2 hours", x, 50 + y);
+    ctx.fillText("Bucket 4: 4 hours", x, 50 + y2);
+    ctx.fillText("Bucket 7: 5 hours", x, 50 + y3);
+    ctx.fillText("Bucket 2: Not used", x2, 50 + y);
+    ctx.fillText("Bucket 5: 1.5 hours", x2, 50 + y2);
+    ctx.fillText("Bucket 8: 30 minutes", x2, 50 + y3);
+    ctx.fillText("Bucket 3: 1 hour", x3, 50 + y);
+    ctx.fillText("Bucket 6: 1 hour", x3, 50 + y2);
+    //print process 2 information
+    ctx.fillText("Bucket 1: 2 hours", x, 200 + y);
+    ctx.fillText("Bucket 2: 4 hours", x, 200 + y2);
+    ctx.fillText("Bucket 3: 5 hours", x, 200 + y3);
+    ctx.fillText("Bucket 4: Not used", x2, 200 + y);
+    ctx.fillText("Bucket 5: 1.5 hours", x2, 200 + y2);
+    ctx.fillText("Bucket 6: 30 minutes", x2, 200 + y3);
+    ctx.fillText("Bucket 7: 1 hour", x3, 200 + y);
+    ctx.fillText("Bucket 8: 1 hour", x3, 200 + y2);
+    //print process 3 information
+    ctx.fillText("Bucket 1: 2 hours", x, 350 + y);
+    ctx.fillText("Bucket 2: 4 hours", x, 350 + y2);
+    ctx.fillText("Bucket 3: 5 hours", x, 350 + y3);
+    ctx.fillText("Bucket 4: Not used", x2, 350 + y);
+    ctx.fillText("Bucket 5: 1.5 hours", x2, 350 + y2);
+    ctx.fillText("Bucket 6: 30 minutes", x2, 350 + y3);
+    ctx.fillText("Bucket 7: 1 hour", x3, 350 + y);
+    ctx.fillText("Bucket 8: 1 hour", x3, 350 + y2);
+    //print process 4 information
+    ctx.fillText("Bucket 1: 2 hours", x, 500 + y);
+    ctx.fillText("Bucket 2: 4 hours", x, 500 + y2);
+    ctx.fillText("Bucket 3: 5 hours", x, 500 + y3);
+    ctx.fillText("Bucket 4: Not used", x2, 500 + y);
+    ctx.fillText("Bucket 5: 1.5 hours", x2, 500 + y2);
+    ctx.fillText("Bucket 6: 30 minutes", x2, 500 + y3);
+    ctx.fillText("Bucket 7: 1 hour", x3, 500 + y);
+    ctx.fillText("Bucket 8: 1 hour", x3, 500 + y2);
+    //print process 5 information
+    ctx.fillText("Bucket 1: 2 hours", x, 650 + y);
+    ctx.fillText("Bucket 2: 4 hours", x, 650 + y2);
+    ctx.fillText("Bucket 3: 5 hours", x, 650 + y3);
+    ctx.fillText("Bucket 4: Not used", x2, 650 + y);
+    ctx.fillText("Bucket 5: 1.5 hours", x2, 650 + y2);
+    ctx.fillText("Bucket 6: 30 minutes", x2, 650 + y3);
+    ctx.fillText("Bucket 7: 1 hour", x3, 650 + y);
+    ctx.fillText("Bucket 8: 1 hour", x3, 650 + y2);
+
+    //right portion of the screen
+    ctx.font = "40px Georgia";
+    ctx.fillText("Change process", 1000, 50 + y3);
+    ctx.fillText("Create new process", 975, 325);
+    ctx.fillText("Select process", 1020, 500);
+}
+
+function create_new_process()
+{
+    let s = [];
+    s.push(`<center><canvas id="canvas" width=100% height=100% style="border:0px solid #FFFFFF;"></canvas></center>`);
+
+    var content = document.getElementById("content");
+    if(content)
+    {
+        content.innerHTML = s.join('');
+    }
+
+    document.body.style.backgroundColor = "lightgrey";
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+}
+
+function change_process()
+{
+    let s = [];
+    s.push(`<center><canvas id="canvas" width=100% height=100% style="border:0px solid #FFFFFF;"></canvas></center>`);
+
+    var content = document.getElementById("content");
+    if(content)
+    {
+        content.innerHTML = s.join('');
+    }
+
+    document.body.style.backgroundColor = "lightgrey";
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+}
+
+function run_process()
+{
+    let s = [];
+    s.push(`<center><canvas id="canvas" width=100% height=100% style="border:0px solid #FFFFFF;"></canvas></center>`);
+
+    var content = document.getElementById("content");
+    if(content)
+    {
+        content.innerHTML = s.join('');
+    }
+
+    document.body.style.backgroundColor = "lightgrey";
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.font = "50px Georgia";
+
+    process_to_run = 1; // get rid of later...
+
+    if(process_to_run == 1)
+    {
+        ctx.fillText("Process 1 is set to run.", 580, 425);
+    }
+    if(process_to_run == 2)
+    {
+        ctx.fillText("Process 2 is set to run.", 580, 425);
+    }
+    if(process_to_run == 3)
+    {
+        ctx.fillText("Process 3 is set to run.", 580, 425);
+    }
+    if(process_to_run == 4)
+    {
+        ctx.fillText("Process 4 is set to run.", 580, 425);
+    }
+    if(process_to_run == 5)
+    {
+        ctx.fillText("Process 5 is set to run.", 580, 425);
+    }
+}
+
+start_screen();
